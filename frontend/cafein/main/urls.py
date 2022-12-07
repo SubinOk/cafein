ab94a7f6bbb9c1a2f9073a86031d455668e7cc37
a024148 <tunta3586@gmail.com>
2022-12-07 09:58:07 +0900

    main urls 불필요한 라인 삭제

--- frontend/cafein/main/urls.py
+++ frontend/cafein/main/urls.py
@@ -2,7 +2,6 @@ from . import views
 from django.urls import path, include
 
 app_name = 'main'
-print("text")
 urlpatterns = [
     path('', views.login, name='login'),
     path('/ownerlogin/', include('owner.urls'))
