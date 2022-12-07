9b4168310ad703fac4745dbea368610bf48bc90d
a024148 <tunta3586@gmail.com>
2022-12-07 14:19:22 +0900

    비밀번호 찾기 부분에서 이메일 분석 부분과 Gmail을 통한 이메일 전송 부분 모듈 추가ㅏ

--- frontend/cafein/owner/views.py
+++ frontend/cafein/owner/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+
 from .forms import loginPostForm
 
 
@@ -14,4 +17,13 @@ def ownerLogin(request):
 
 
 def findPassword(request):
-    return render(request, 'findPassword.html')
+    if request.method == 'POST':
+
+        # 일치 하는 이메일이 있는 경우 이메일 전송 방법 setting은 cafein settings.py 참조
+        # to_email = 'tunta3586@gmail.com'
+        # send_email = EmailMessage('Subject here', 'Here is the message', to=[to_email])
+        # send_email.send()
+        return render(request, 'findPassword.html', {'flg': True})
+    else:
+        return render(request, 'findPassword.html', {'flg': False})
+
