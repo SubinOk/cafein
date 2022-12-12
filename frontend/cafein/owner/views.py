e8b6f917b8071443f3cb92d3c67f032a5dad9e08
a024148 <tunta3586@gamil.com>
2022-12-12 11:03:32 +0900

    signup시 리턴 페이지 수정

--- frontend/cafein/owner/views.py
+++ frontend/cafein/owner/views.py
@@ -87,7 +87,7 @@ def signup(request):
         form = signupPostForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'ownerHome.html')
+        return render(request, 'ownerHome.html')
     else :
         form = signupPostForm()
         return render(request, 'signup.html', {'form':form})
