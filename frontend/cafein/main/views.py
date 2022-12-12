9092e5882a2145c71a5737ee6b470c49deaab62b
a024148 <tunta3586@gamil.com>
2022-12-12 15:53:28 +0900

    메인페이지 접속시 세션유무 확인

--- frontend/cafein/main/views.py
+++ frontend/cafein/main/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def mainPage(request):
+    if request.session.get('user'):
+        return redirect('/owner/home')
     return render(request, 'mainPage.html')
 
 def login(request):
