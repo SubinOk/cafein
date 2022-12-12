406036a852017546f509ed1fd0700e2c50afe0e2
a024148 <tunta3586@gamil.com>
2022-12-12 11:36:33 +0900

    메인 페이지 추가에 따른 경로 및 html 수정

--- frontend/cafein/main/urls.py
+++ frontend/cafein/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 app_name = 'main'
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.mainPage, name='mainPage'),
+    path('login/', views.login, name='login'),
 ]
--- frontend/cafein/main/views.py
+++ frontend/cafein/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mainPage(request):
+    return render(request, 'mainPage.html')
+
 def login(request):
     return render(request, 'login.html')
