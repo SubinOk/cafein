4b077d219c6e419011c0c4230e616f3599fa5dce
dpdiver <dommodommo28@gmail.com>
2022-12-30 18:36:17 +0900

    즐겨찾기, customer url 수정

--- frontend/cafein/customer/urls.py
+++ frontend/cafein/customer/urls.py
@@ -6,24 +6,12 @@ app_name = 'customer'
 urlpatterns =[
     path('home/', views.customerHome, name='customerHome'),
     path('signup/', views.signup, name='signup'),
-    path('<cafeName>/home', views.cafeHome, name='cafeHome'),
+    path('<int:cafeId>/home', views.cafeHome, name='cafeHome'),
     path('review/', views.cafeReview, name='cafeReview'),
-    path('<cafeName>/createReview', views.createReview, name='createReview'),
-    path('<cafeName>/review/<int:reviewid>', views.cafeReviewDetail, name='cafeReviewDetail'),
-    path('like/<cafeName>', views.cafeLike, name='cafeLike'),
+    path('<int:cafeId>/createReview', views.createReview, name='createReview'),
+    path('<int:cafeId>/review/<int:reviewid>', views.cafeReviewDetail, name='cafeReviewDetail'),
+    path('like/<int:cafeId>', views.cafeLike, name='cafeLike'),
     path('like/', views.index, name='cafeLikeList'),
     path('findCafe/', views.getCafeData, name='getCafeData'),
 ]
 
-# urlpatterns = [
-#     path('login/', views.ownerLogin, name='ownerLogin'),
-#     path('logout/', views.ownerLogout, name='ownerLogout'),
-#     path('home/', views.ownerHome, name='ownerHome'),
-#     path('checkPassword/', views.checkPassword, name='checkPassword'),
-#     path('signup/', views.signup, name='signup'),
-#     path('delete/', views.ownerDelete, name='ownerDelete'), 
-#     path('change/', views.ownerChange, name='ownerChange'),
-#     path('manage/', views.ownerManage, name='ownerManage'),
-#     path('statistics/', views.ownerStatistics, name='ownerStatistics'),
-#     path('statistics/detail', views.ownerStatisticsDetail, name='ownerStatisticsDetail'),
-# ]
