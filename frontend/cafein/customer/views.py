f1ec808d41decdf9f3c69d65a1edfb5c771ceb61
SubinOk <duddldjssl@naver.com>
2023-01-03 11:10:25 +0900

    댓글명 수정 및 리뷰 최신순 정렬

--- frontend/cafein/customer/views.py
+++ frontend/cafein/customer/views.py
@@ -86,13 +86,13 @@ def cafeHome(request, cafeId):
     else:
         cafe = Cafe.objects.get(cafe_id=cafeId)
         customer = User.objects.filter(email=request.session.get('user'))[0]
-        cafe_reviews = Cafe_review.objects.filter(cafe=cafe)[:3]
+        cafe_reviews = Cafe_review.objects.filter(cafe=cafe).order_by('-date')[:3]
         cafe_menu = Cafe_menu.objects.filter(cafe=cafe)
     return render(request, 'cafeHome.html', {'cafe': cafe, 'customer':customer, 'reviews':cafe_reviews, 'cafe_menu': cafe_menu, 'title': cafe.name+' 홈'})
 
 def cafeReview(request):
     
-    cafe_reviews = Cafe_review.objects.all()
+    cafe_reviews = Cafe_review.objects.all().order_by('-date')
 
     paginator = Paginator(cafe_reviews, 2)
 
