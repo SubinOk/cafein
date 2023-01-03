20672e1975ba5ffd942a0807455374a02ef7bdd6
nuri <knr08230@naver.com>
2023-01-03 18:53:08 +0900

    카페리뷰 다시분석한 결과값 db에 다시 저장하는 코드

---
+++ frontend/cafein/cafe/management/commands/reSentiment.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from itertools import islice
+from django.core.management.base import BaseCommand
+from cafe.models import Cafe_sentiment,Cafe,Cafe_rank,Cafe_congestion
+from django.core.files import File
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+    
+        batch_size = 100
+        column_total = ['cafe_name','total_pn','total_count']
+        column_category = ['category','rank','sentiment','cnt','positive_cnt']
+        column = ['people']
+        df_total = pd.read_csv('cafein/files/df_total.csv', names = column_total, header=0)
+        df_category = pd.read_csv ('cafein/files/df_category.csv', names = column_category, header=0)
+        df_total = df_total.fillna(0)
+        df_category = df_category.fillna(0)
+        name = df_total.iloc[0,0] 
+        cafe = Cafe.objects.filter(name = name)
+
+        cafe_sentiment=Cafe_sentiment.objects.filter(cafe = cafe[0])[0]
+        cafe_sentiment.delete()
+
+        print(1)
+        #긍부정 저장
+        objs = (Cafe_sentiment(
+            total = row[1],
+            cafe = cafe[0],
+            total_count = row[2]
+        ) for _, row in df_total.iterrows() )
+        while True:
+            batch = list(islice(objs, batch_size))
+            if not batch:
+                break
+            Cafe_sentiment.objects.bulk_create(batch, batch_size)
+        sentiment = Cafe_sentiment.objects.filter(cafe = cafe[0])
+
+        print(2)
+
+        cafe_rank=Cafe_rank.objects.filter(sentiment = sentiment[0])
+        print(cafe_rank)
+        cafe_rank.delete()
+
+        print(3)
+        #리뷰 순위 저장
+        rank = (Cafe_rank(
+            category = row[0],
+            rank = row[1],
+            ratio = row[2],
+            sentiment = sentiment[0],
+            cnt = row[3],
+            positive_cnt = row[4]
+
+        ) for _, row in df_category.iterrows() )
+        while True:
+            batch = list(islice(rank, batch_size))
+            if not batch:
+                break
+            Cafe_rank.objects.bulk_create(batch, batch_size)
+
+        print(4)
+        df = pd.read_csv('cafein/files/result/temp.csv', names = column, header=0)
+        df = df.fillna(0)
+        cafe = Cafe.objects.filter(name = name)
+        max =cafe[0].max_occupancy
+
+        cafe_congestion=Cafe_congestion.objects.filter(cafe =cafe[0])[0]
+        cafe_congestion.delete()
+
+        print(5)
+        #혼잡도 저장
+        objs = (Cafe_congestion(
+            congestion = (row[0] / max* 100.0),
+            cafe = cafe[0],
+        ) for _, row in df.iterrows() )
+        while True:
+            batch = list(islice(objs, batch_size))
+            if not batch:
+                break
+            Cafe_congestion.objects.bulk_create(batch, batch_size)
+        print(6)    
+
+        
